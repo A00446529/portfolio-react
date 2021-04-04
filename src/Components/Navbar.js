47b1acf7287a8f5bbd72c1c3ff81fa6ed3106759
A00446529 <dipesh.timilsina@smu.ca>
2021-04-03 22:21:56 -0300

    Started working on first block

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,6 @@ import logo from "../Logo.png"
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-
 <div className="container">
   <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="Portfolio Logo"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,12 +10,24 @@ const Navbar = () => {
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="na vbar-nav ml-auto">
+    <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">About Me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">My Projects</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
