3bed4238e520055015926e718483826ec194b54e
A00446529 <dipesh.timilsina@smu.ca>
2021-04-08 18:53:02 -0300

    Added typed and installed particles package

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,19 @@ import Typed from "react-typed";
 
 const Header = () => {
     return (
-        <div className="header-wraper">
+        <div className= "header-wraper">
             <div className="main-info">
-                <h1>Website Devolopment and Cloud Computing</h1>
+                <h1>Web Development and Cloud Computing</h1>
                 <Typed
                     className="typed-text"
-                    strings={["Web Deisgn", "Web Development", "Cloud Computing", "Database Design"]}
+                    strings={["Web Design", "Web Development", "Cloud Computing", "Database Design"]}
+                    typeSpeed={40}
+                    backSpeed={20}
+                    loop 
                     />
+         <a href= "#" className="btn-main-contact">Contact Me</a>
             </div>
+            
         </div>
     )
 }
