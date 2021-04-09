e5373ba692085340196a9b6400d8a3b5f2411a77
A00446529 <dipesh.timilsina@smu.ca>
2021-04-09 15:59:01 -0300

    Weather finally showing right data

--- src/Containers/City.js
+++ src/Containers/City.js
@@ -1,31 +1,42 @@
 import React from 'react';
 
 export default class City extends React.Component{
-    // state= {
-    //     city : 'Kathmandu'
-    // }
-
+    constructor(props){
+        super (props)
+        this.state = {
+            temperature: ''
+        }
+        this.getTemperature= this.getTemperature.bind(this);
+    }
+    
     getTemperature= () => {
         const cityname= 'kathmandu';
-        const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=d8c35d230949431c37b2040055ce4e95"
+        const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid=d8c35d230949431c37b2040055ce4e95&units=metric"
         fetch(url).then(response => {
             return response.json();
-        }).then(data => {
-            console.log(data.main.temp);
+        }).then(response => {
+            //console.log(response.main.temp);
+            this.setState({
+                temperature: response.main.temp
+            })
+
         }).catch(error => {
             console.log(error);
         })
       }
         
+      componentDidMount(){
+          this.getTemperature()
+      }
     render(){
-        let data= null;
-        data= <p>temperature is :{this.state.temperature}</p>
         return(
             <div style={{textAlign: 'center', fontSize: '17px'}}>
             <p>Weather right now</p>
             <button onClick={this.getTemperature}>Get Temperature</button>
-
+            <p><b>Current Temperature: </b>{this.state.temperature}</p>
             </div>
         )
+
     };
+
 } 
